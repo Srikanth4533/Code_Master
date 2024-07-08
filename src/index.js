f169571a0852aa3dcac1da4ffc7fae1050fe4f2f
Srikanth4533 <kondakallasrikanth991@gmail.com>
2024-07-08 14:08:44 +0530

    Server setup done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,37 @@
 const express = require("express")
+const bodyParser = require("body-parser")
 
 const { PORT } = require("./config/serverConfig")
 
 const app = express()
 
-app.listen(PORT, () => {
-    console.log(`Server started at port ${PORT}`)
+process.on("uncaughtException", (err) => {
+    console.log(`Error Name: ${err.name} Error: ${err.message}`)
+    console.log(`Shutting down the server due to Uncaught Exception`)
+
+    process.exit(1)
+
 })
+
+
+
+const setupAndStart = () => {
+
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({ extended: true }))
+
+    const server = app.listen(PORT, () => {
+        console.log(`Server started at port ${PORT}`)
+    })
+
+    process.on("unhandledRejection", (err) => {
+        console.log(`Error Name: ${err.name} Error: ${err.message}`)
+        console.log(`Shutting down the server due to Unhandled Promise Rejection`)
+
+        server.close(() => {
+            process.exit(1)
+        })
+    })
+}
+
+setupAndStart()
